86ae7daf01ed2a8cc066e3bd1f6c8008789a7009
Will Woodard <willcwoodard@gmail.com>
2020-05-02 13:50:44 -0500

    Initial Commit

---
+++ touchTest6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  touchTest6
+//
+//  Created by Will Woodard on 5/2/20.
+//  Copyright © 2020 Will Woodard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ touchTest6Tests/touchTest6Tests.swift
@@ -0,0 +1,34 @@
+//
+//  touchTest6Tests.swift
+//  touchTest6Tests
+//
+//  Created by Will Woodard on 5/2/20.
+//  Copyright © 2020 Will Woodard. All rights reserved.
+//
+
+import XCTest
+@testable import touchTest6
+
+class touchTest6Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
